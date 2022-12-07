a5b4216b34d85948b6cb7a9ca4d9df824f4e5971
Dylan Nicolini <Dylan.Nicolini@Gmail.com>
2022-12-07 16:09:37 -0500

    added unit tests project and folder

---
+++ api/apitests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace apitests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
